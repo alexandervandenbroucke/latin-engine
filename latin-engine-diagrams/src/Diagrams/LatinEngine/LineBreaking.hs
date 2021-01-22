6bb741a2bbb6a4d50ae2ba1d71fc66e14e485de7
Alexander Vandenbroucke <alexander.vandenbroucke@kuleuven.be>
2021-01-22 09:52:09 +0100

    Refactor line-breaking.

--- latin-engine-diagrams/src/Diagrams/LatinEngine/LineBreaking.hs
+++ latin-engine-diagrams/src/Diagrams/LatinEngine/LineBreaking.hs
@@ -24,14 +24,19 @@ It does this in worst-case quadratic time, but is mostly linear in practice.
 However, the implementation in this module is not optimised, and it is likely
 that a significant reduction in the constant factors could be achieved.
 
+If you simply want to use this module to break a sentence into lines, call
+'break' with the desired line length, badness tolerance, parse forest and
+sentence.
+
 -}
 
 module Diagrams.LatinEngine.LineBreaking
-  ( -- * Lines
+  ( -- * Lines and Metrics
     Line(..),
     len,
     sentenceBadness,
     forestDemerit,
+    Metrics(..),
     -- * Candidates
     Candidate(..),
     initCandidate, extend, chain,
@@ -54,9 +59,18 @@ import           Prelude hiding (break)
 import qualified Data.Sentence as S
 import qualified Data.Forest as F
 
+-------------------------------------------------------------------------------
+-- Lines and Metrics
 
 -- | A line, given a start (inclusive) and end point (exclusive).
 data Line = Line !S.WordId !S.WordId
+
+-- | A gives the badness, badness tolerance and demerits of a line
+data Metrics = Metrics {
+  badness   :: Line -> Int,
+  demerits  :: Line -> Int,
+  tolerance :: !Int
+}
 
 -- | The character count of a 'Line'.
 len :: S.Sentence -> Line -> Int
@@ -67,6 +81,9 @@ len sentence (Line start end) = maybe 0 finalise (traverse go [start..(end-1)])
     go w = fmap (T.length . S.wordText) (S.wordNr w sentence)
 
 -- | Badness of a 'Line' given a desired length.
+--
+-- Negative badness means that the line is too long, postive badness means that
+-- the line is too short.
 sentenceBadness
   :: S.Sentence -- ^ the 'S.Sentence' that the 'Line' is a part of
   -> Int        -- ^ the desired length
@@ -82,17 +99,17 @@ sentenceBadness sentence desired line = desired - len sentence line
 forestDemerit :: F.Forest -> Line -> Int
 forestDemerit forest (Line start end) =
   let line = [start .. end - 1]
-      outbound =
-        [ word
-        | word <- line,
-          F.Child parent <- [F.statusOf forest word],
-          parent >= end]
-      inbound =
-        [ child
-        | word <- line,
-          child <- Set.toList $ F.children word forest,
-          child >= end]
-  in (length outbound + length inbound)^3
+      outbound = length (filter farParent line) where
+        farParent word = case F.statusOf forest word of
+          F.Child parent -> parent >= end
+          _ -> False
+      inbound = sum (map (Set.size . farChildren) line) where
+        farChildren word = Set.filter (>= end) (F.children word forest)
+  in (outbound + inbound)^3
+
+-------------------------------------------------------------------------------
+-- Candidates
+
 
 -- | A candidate breakpoint.
 --
@@ -105,6 +122,7 @@ data Candidate = Candidate {
 } deriving (Eq,Ord)
 -- note: in theory we don't need to keep a pointer to every previous candidate
 -- a chain (list strict in the head) to previous word ids would suffice.
+-- But this is useful for debugging.
 
 instance Show Candidate where
   show (Candidate d b p)
@@ -137,26 +155,27 @@ initCandidate :: Candidate
 initCandidate = Candidate 0 0 initCandidate
 
 
+--------------------------------------------------------------------------------
+-- Actual line breaking
+
 -- | Update a set of active 'Candidate's.
 --
 updateCandidates
-  :: (Line -> Int) -- ^ badness function
-  -> (Line -> Int) -- ^ demerit function
-  -> Int           -- ^ tolerance
+  :: Metrics       -- ^ metrics
   -> [Candidate]   -- ^ current active candidate set
   -> S.WordId      -- ^ current break point
   -> [Candidate]   -- ^ new active candidate set
-updateCandidates badness demerit tolerance active breakpoint =
+updateCandidates metrics active breakpoint =
   let line prev = Line (candidateBreak prev + 1) (breakpoint + 1)
       -- a line that includes the words after the last breakpoint, up to
       -- and including the word right before the new break point.
-      stale c = badness (line c) < -tolerance
+      stale c = badness metrics (line c) < -(tolerance metrics)
       potential =
         [extend breakpoint d prev
         | prev <- active,
-          let b = badness (line prev),
-          let d = 10*demerit (line prev) + abs b,
-          abs b <= tolerance]
+          let b = badness metrics (line prev),
+          let d = 10*demerits metrics (line prev) + abs b,
+          abs b <= tolerance metrics]
       newCandidate
         | null potential = []
         | otherwise = [L.minimumBy (comparing totalDemerit) potential]
@@ -165,30 +184,28 @@ updateCandidates badness demerit tolerance active breakpoint =
 -- | The list of active candidate break points for a sentence.
 --
 --
--- @candidates d t forest sentence@ returns the set of active candidates
--- after breaking @sentence@ into lines, using @forest@ to compute demerits,
--- with desired line length @d@ and tolerance @t@.
+-- @candidates metrics sentence@ returns the set of active candidates
+-- after breaking @sentence@ into lines, using @metrics@ to compute demerits,
+-- badness and tolerance.
 --
 -- The first element in this list is the last candidate that was found, i.e.
 -- which breaks the list the furthest. This is usually the candidate that
 -- you want.
-candidates :: Int -> Int -> F.Forest -> S.Sentence -> NonEmpty Candidate
-candidates desired tolerance forest sentence =
-  let bd = sentenceBadness sentence desired
-      dm = forestDemerit forest
-      ws = [1..S.wordCount sentence]
-      update cs w =
-        case NE.nonEmpty (updateCandidates bd dm tolerance (NE.toList cs) w) of
-          Just newCs -> newCs
-          Nothing -> extend w 0 (NE.head cs) :| []
-          -- If the candidate list becomes empty, that means that we couldn't
-          -- find a breakpoint that didn't exceed the badness tolerance, and
-          -- all previous breakpoints have become stale. In that case, we set
-          -- an overful line, and reset the demerit score. We don't need more
-          -- accurate demerit accounting, because all later breakpoints must
-          -- start from this breakpoint. Hence, their relative total demerits
-          -- are still correct.
-  in L.foldl' update (initCandidate :| []) ws
+candidates :: Metrics -> S.Sentence -> NonEmpty Candidate
+candidates metrics sentence =
+  let update cs w =
+        let updated = updateCandidates metrics (NE.toList cs) w
+        in case NE.nonEmpty updated of
+             Just newCs -> newCs
+             Nothing -> extend w 0 (NE.head cs) :| []
+        -- If the candidate list becomes empty, that means that we couldn't
+        -- find a breakpoint that didn't exceed the badness tolerance, and
+        -- all previous breakpoints have become stale. In that case, we set
+        -- an overful line, and reset the demerit score. We don't need more
+        -- accurate demerit accounting, because all later breakpoints must
+        -- start from this breakpoint. Hence, their relative total demerits
+        -- are still correct.
+  in L.foldl' update (initCandidate :| []) [1..S.wordCount sentence-1]
 
 -- | The list of optimal break points (fewest total demerits) of a sentence.
 --
@@ -198,14 +215,17 @@ candidates desired tolerance forest sentence =
 -- 
 breakpoints :: Int -> Int -> F.Forest -> S.Sentence -> [Candidate]
 breakpoints desired tolerance forest sentence =
-  let w = S.wordCount sentence
-      cs = candidates desired tolerance forest sentence
-      dm = forestDemerit forest
-      bd = sentenceBadness sentence desired
-      lastBreakPoint = L.minimumBy (comparing totalDemerit) ps where
-        ps = [extend w (10 * dm l + abs (min 0 (bd l))) prev
-             | prev <- NE.toList cs,
-               let l = Line (candidateBreak prev + 1) (w + 1)
+  let wordCount = S.wordCount sentence
+      metrics = Metrics{
+        badness = sentenceBadness sentence desired,
+        demerits = forestDemerit forest,
+        tolerance = tolerance
+        }
+      lastBreakPoint = L.minimumBy (comparing totalDemerit) cs where
+        cs = [extend wordCount b prev
+             | prev <- NE.toList (candidates metrics sentence),
+               let l = Line (candidateBreak prev + 1) (wordCount + 1),
+               let b = 10*demerits metrics l - min 0 (badness metrics l)
              ]
       -- The last break point is chosen without looking at positive badness.
       -- This means that the last breakpoint is simply chosen to minimise total
